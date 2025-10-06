0aa0c44b130804a40bf15ee969e45a6e51bed34a
Javier Baena Martin <jbaemar@upv.edu.es>
2025-10-07 00:13:46 +0200

    busqueda inversa añadida

--- DyV.h
+++ DyV.h
@@ -3,6 +3,26 @@
 
 using namespace std;
 
+int BusquedaInv(int x, vector<int>& vec, int ini, int fin){
+	if(ini < fin){
+		return -1;
+	}
+	
+	int medio = (ini + fin)/2;
+	if(vec[medio] == x){
+		return medio;
+	}else if(vec[medio] > x){
+		return BusquedaInv(x, vec, medio+1, fin);
+	}else if(vec[medio] < x){
+		return BusquedaInv(x, vec, ini, medio-1);
+	}
+	return -1;
+
+}
+
+
+
+
 int Busqueda(int x, vector<int>& vec, int ini, int fin){
 	if(ini>fin){
 		return -1;
@@ -15,6 +35,7 @@ int Busqueda(int x, vector<int>& vec, int ini, int fin){
 	}else if(vec[medio]<x){
 		return Busqueda(x,vec,medio+1,fin);
 	}
+	return -1;
 	
 }
 
--- test_DyV.cpp
+++ test_DyV.cpp
@@ -38,16 +38,31 @@ int main(){
 	}
 	
 	sort(vec.begin(),vec.end());
+
+	cout << "Quiere el vector ordenado de forma ascendente (1) o descendente (2)? ";
+	int orden;
+	cin >> orden;
 	cout << "El vector ordenado es: ";
+	if(orden == 2){
+		reverse(vec.begin(),vec.end());
+	}else if(orden != 1){
+		cout << "por favor, introduzca un valor válido\n";
+		return 0;
+	}
 	for(int i=0; i<tam; i++){
 		cout << vec[i] << " ";
 	}
 	cout << endl;
 	
+
 	cout << "Introduce el valor a buscar: ";
 	cin >> valor;
- 
-	int pos = Busqueda(valor,vec,0,tam-1);
+	int pos;
+	if(orden == 1){
+		pos = Busqueda(valor,vec,0,tam-1);
+	}else{
+		pos = BusquedaInv(valor,vec,0,tam-1);
+	}
 	if(pos = -1){
 		cout << "El valor introducido no se encuentra en el vector \n";
 	}else{
